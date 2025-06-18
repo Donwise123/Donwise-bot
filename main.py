c2ad466281247e5b538e71bfd36f8ccf88c1916e
Donwise123 <donwise1123@gmail.com>
2025-06-19 00:47:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+
+from telethon import TelegramClient, events
+import os
+from keep_alive import keep_alive
+
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
+session_name = 'donwise_session'
+
+source_channels = [
+    'https://t.me/firepipsignals',
+    'https://t.me/Forex_Top_Premium_Signals',
+    'https://t.me/forexsignals01_trade',
+    'https://t.me/forexgdp0',
+    'https://t.me/Goldforexsignalfx11',
+    'https://t.me/habbyforex',
+    'https://t.me/kojoforextrades'
+]
+
+target_channel = 'https://t.me/+LWjMM6W7LtdjOTM0'
+
+keywords = ['buy', 'sell', 'tp', 'sl', 'xauusd', 'gold', 'nas100', 'eurusd', 'gbpusd']
+signature = "\n\nForwarded by @RealDonwise 🔥 | Donwise Copytrade Vault"
+
+client = TelegramClient(session_name, api_id, api_hash)
+
+@client.on(events.NewMessage(chats=source_channels))
+async def forward_signal(event):
+    text = event.raw_text.lower()
+    if any(k in text for k in keywords):
+        if event.message.media:
+            await client.send_file(target_channel, event.message.media, caption=(event.message.message or "") + signature)
+        else:
+            await client.send_message(target_channel, event.message.message + signature)
+        print("✅ Signal Forwarded")
+
+keep_alive()
+client.start()
+client.run_until_disconnected()
